4c3307bd49d122aa764a7fdbb0c49555f50b14b3
Lemuel Horas <lemuelhorasdaniel@gmail.com>
2025-12-01 23:03:06 +0700

    fixed early stop callback #3

--- model/no_more_sw/blocks/custom_callbacks.py
+++ model/no_more_sw/blocks/custom_callbacks.py
@@ -87,7 +87,7 @@ class StopAtEpoch(Callback):
         self.stop_epoch = trainer.current_epoch + self.stop_epoch
 
     def on_validation_epoch_end(self, trainer, pl_module):
-        current_epoch = trainer.current_epoch + 1
+        current_epoch = trainer.current_epoch
         if current_epoch >= self.stop_epoch:
             trainer.should_stop = True
 
