374a36f94560aa436bdcd827bdf75c6cdbbad2a8
Lemuel Horas <lemuelhorasdaniel@gmail.com>
2025-12-23 12:05:07 +0700

    3 block mednext

--- model/backbones/mednext/net.py
+++ model/backbones/mednext/net.py
@@ -45,7 +45,7 @@ class MedNeXt(Seg3D):
         dim="3d",  # 2d or 3d
         grn=False,
         loss_args={
-            "name": "DiceCELossNNUNET",
+            "name": "DiceCELossMONAI",
         },
     ):
 
@@ -207,7 +207,34 @@ class Encoder(nn.Module):
             grn=grn,
         )
 
-        self.enc_block_3 = nn.Sequential(
+        # self.enc_block_3 = nn.Sequential(
+        #     *[
+        #         MedNeXtBlock(
+        #             in_channels=n_channels * 8,
+        #             out_channels=n_channels * 8,
+        #             exp_r=exp_r[3],
+        #             kernel_size=enc_kernel_size,
+        #             do_res=do_res,
+        #             norm_type=norm_type,
+        #             dim=dim,
+        #             grn=grn,
+        #         )
+        #         for i in range(block_counts[3])
+        #     ]
+        # )
+
+        # self.down_3 = MedNeXtDownBlock(
+        #     in_channels=8 * n_channels,
+        #     out_channels=16 * n_channels,
+        #     exp_r=exp_r[4],
+        #     kernel_size=enc_kernel_size,
+        #     do_res=do_res_up_down,
+        #     norm_type=norm_type,
+        #     dim=dim,
+        #     grn=grn,
+        # )
+
+        self.bottleneck = nn.Sequential(
             *[
                 MedNeXtBlock(
                     in_channels=n_channels * 8,
@@ -220,33 +247,6 @@ class Encoder(nn.Module):
                     grn=grn,
                 )
                 for i in range(block_counts[3])
-            ]
-        )
-
-        self.down_3 = MedNeXtDownBlock(
-            in_channels=8 * n_channels,
-            out_channels=16 * n_channels,
-            exp_r=exp_r[4],
-            kernel_size=enc_kernel_size,
-            do_res=do_res_up_down,
-            norm_type=norm_type,
-            dim=dim,
-            grn=grn,
-        )
-
-        self.bottleneck = nn.Sequential(
-            *[
-                MedNeXtBlock(
-                    in_channels=n_channels * 16,
-                    out_channels=n_channels * 16,
-                    exp_r=exp_r[4],
-                    kernel_size=enc_kernel_size,
-                    do_res=do_res,
-                    norm_type=norm_type,
-                    dim=dim,
-                    grn=grn,
-                )
-                for i in range(block_counts[4])
             ]
         )
 
@@ -285,9 +285,9 @@ class Encoder(nn.Module):
             x = checkpoint.checkpoint(self.down_2, x_res_2, self.dummy_tensor)
             features.append(x_res_2)
 
-            x_res_3 = self.iterative_checkpoint(self.enc_block_3, x)
-            x = checkpoint.checkpoint(self.down_3, x_res_3, self.dummy_tensor)
-            features.append(x_res_3)
+            # x_res_3 = self.iterative_checkpoint(self.enc_block_3, x)
+            # x = checkpoint.checkpoint(self.down_3, x_res_3, self.dummy_tensor)
+            # features.append(x_res_3)
 
             x = self.iterative_checkpoint(self.bottleneck, x)
             features.append(x)
@@ -305,9 +305,9 @@ class Encoder(nn.Module):
             x = self.down_2(x_res_2)
             features.append(x_res_2)
 
-            x_res_3 = self.enc_block_3(x)
-            x = self.down_3(x_res_3)
-            features.append(x_res_3)
+            # x_res_3 = self.enc_block_3(x)
+            # x = self.down_3(x_res_3)
+            # features.append(x_res_3)
 
             x = self.bottleneck(x)
             features.append(x)
@@ -343,37 +343,37 @@ class Decoder(nn.Module):
 
         self.outside_block_checkpointing = outside_block_checkpointing
 
-        self.up_3 = MedNeXtUpBlock(
-            in_channels=16 * n_channels,
-            out_channels=8 * n_channels,
-            exp_r=exp_r[0],
-            kernel_size=kernel_size,
-            do_res=do_res_up_down,
-            norm_type=norm_type,
-            dim=dim,
-            grn=grn,
-        )
+        # self.up_3 = MedNeXtUpBlock(
+        #     in_channels=16 * n_channels,
+        #     out_channels=8 * n_channels,
+        #     exp_r=exp_r[0],
+        #     kernel_size=kernel_size,
+        #     do_res=do_res_up_down,
+        #     norm_type=norm_type,
+        #     dim=dim,
+        #     grn=grn,
+        # )
 
-        self.dec_block_3 = nn.Sequential(
-            *[
-                MedNeXtBlock(
-                    in_channels=n_channels * 8,
-                    out_channels=n_channels * 8,
-                    exp_r=exp_r[0],
-                    kernel_size=kernel_size,
-                    do_res=do_res,
-                    norm_type=norm_type,
-                    dim=dim,
-                    grn=grn,
-                )
-                for i in range(block_counts[0])
-            ]
-        )
+        # self.dec_block_3 = nn.Sequential(
+        #     *[
+        #         MedNeXtBlock(
+        #             in_channels=n_channels * 8,
+        #             out_channels=n_channels * 8,
+        #             exp_r=exp_r[0],
+        #             kernel_size=kernel_size,
+        #             do_res=do_res,
+        #             norm_type=norm_type,
+        #             dim=dim,
+        #             grn=grn,
+        #         )
+        #         for i in range(block_counts[0])
+        #     ]
+        # )
 
         self.up_2 = MedNeXtUpBlock(
             in_channels=8 * n_channels,
             out_channels=4 * n_channels,
-            exp_r=exp_r[1],
+            exp_r=exp_r[0],
             kernel_size=kernel_size,
             do_res=do_res_up_down,
             norm_type=norm_type,
@@ -386,21 +386,21 @@ class Decoder(nn.Module):
                 MedNeXtBlock(
                     in_channels=n_channels * 4,
                     out_channels=n_channels * 4,
-                    exp_r=exp_r[1],
+                    exp_r=exp_r[0],
                     kernel_size=kernel_size,
                     do_res=do_res,
                     norm_type=norm_type,
                     dim=dim,
                     grn=grn,
                 )
-                for i in range(block_counts[1])
+                for i in range(block_counts[0])
             ]
         )
 
         self.up_1 = MedNeXtUpBlock(
             in_channels=4 * n_channels,
             out_channels=2 * n_channels,
-            exp_r=exp_r[2],
+            exp_r=exp_r[1],
             kernel_size=kernel_size,
             do_res=do_res_up_down,
             norm_type=norm_type,
@@ -413,21 +413,21 @@ class Decoder(nn.Module):
                 MedNeXtBlock(
                     in_channels=n_channels * 2,
                     out_channels=n_channels * 2,
-                    exp_r=exp_r[2],
+                    exp_r=exp_r[1],
                     kernel_size=kernel_size,
                     do_res=do_res,
                     norm_type=norm_type,
                     dim=dim,
                     grn=grn,
                 )
-                for i in range(block_counts[2])
+                for i in range(block_counts[1])
             ]
         )
 
         self.up_0 = MedNeXtUpBlock(
             in_channels=2 * n_channels,
             out_channels=n_channels,
-            exp_r=exp_r[3],
+            exp_r=exp_r[2],
             kernel_size=kernel_size,
             do_res=do_res_up_down,
             norm_type=norm_type,
@@ -440,14 +440,14 @@ class Decoder(nn.Module):
                 MedNeXtBlock(
                     in_channels=n_channels,
                     out_channels=n_channels,
-                    exp_r=exp_r[3],
+                    exp_r=exp_r[2],
                     kernel_size=kernel_size,
                     do_res=do_res,
                     norm_type=norm_type,
                     dim=dim,
                     grn=grn,
                 )
-                for i in range(block_counts[3])
+                for i in range(block_counts[2])
             ]
         )
 
@@ -469,13 +469,13 @@ class Decoder(nn.Module):
 
     def forward(self, features):
 
-        x_res_0, x_res_1, x_res_2, x_res_3, x = features
+        x_res_0, x_res_1, x_res_2, x = features
 
         if self.outside_block_checkpointing:
 
-            x_up_3 = checkpoint.checkpoint(self.up_3, x, self.dummy_tensor)
-            dec_x = x_res_3 + x_up_3
-            x = self.iterative_checkpoint(self.dec_block_3, dec_x)
+            # x_up_3 = checkpoint.checkpoint(self.up_3, x, self.dummy_tensor)
+            # dec_x = x_res_3 + x_up_3
+            # x = self.iterative_checkpoint(self.dec_block_3, dec_x)
             # del x_res_3, x_up_3
 
             x_up_2 = checkpoint.checkpoint(self.up_2, x, self.dummy_tensor)
@@ -496,9 +496,9 @@ class Decoder(nn.Module):
             x = checkpoint.checkpoint(self.out_0, x, self.dummy_tensor)
 
         else:
-            x_up_3 = self.up_3(x)
-            dec_x = x_res_3 + x_up_3
-            x = self.dec_block_3(dec_x)
+            # x_up_3 = self.up_3(x)
+            # dec_x = x_res_3 + x_up_3
+            # x = self.dec_block_3(dec_x)
             # del x_res_3, x_up_3
 
             x_up_2 = self.up_2(x)
@@ -518,7 +518,7 @@ class Decoder(nn.Module):
 
             x = self.out_0(x)
 
-        return [*features, x_up_3, x_up_2, x_up_1, x_up_0, x]
+        return [*features, x_up_2, x_up_1, x_up_0, x]
 
 
 @backbone_registry.register
